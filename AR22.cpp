6331a6c177a6fc1c2d0477ae25f085b1970d804e
Ricky-Henson <82556503+Ricky-Henson@users.noreply.github.com>
2022-06-20 13:13:55 +0800

    Update AR22.cpp

--- AR22.cpp
+++ AR22.cpp
@@ -1,1 +1,15 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
 
+int main(){
+    string text;
+
+	cin >> text;
+	for(int i=0; i<text.length(); i++){
+		text[i]-=3;
+	}
+	cout << text << endl;
+
+    return 0;
+}
