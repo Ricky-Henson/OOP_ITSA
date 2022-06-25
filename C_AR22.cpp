d8148b8432977e86160e1df70cfa88b15ac8dd83
Ricky-Henson <82556503+Ricky-Henson@users.noreply.github.com>
2022-06-25 12:04:45 +0800

    Update C_AR22.cpp

--- C_AR22.cpp
+++ C_AR22.cpp
@@ -1,21 +1,36 @@
-#include <bits/stdc++.h>
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	string str1, str2;
-	getline(cin, str1);
-	getline(cin, str2);
-
-	for(int i=0; i<str.length(); i++){
-		if(isupper(str[i])) alphabet[str[i] - 'A']++;
-		else if(islower(str[i])) alphabet[str[i] - 'a']++;
-	}
-	for(int i=0; i<26; i++){
-		if(i<25)
-			cout << alphabet[i] << " ";
-		else cout << alphabet[i] << endl;
-	}
-	return 0;
-}
+#include <bits/stdc++.h>  
+#include <iostream>  
+using namespace std;  
+  
+int compare(const void* a, const void* b)  
+{  
+    const int* x = (int*) a;  
+    const int* y = (int*) b;  
+  
+    if (*x > *y)  
+        return 1;  
+    else if (*x < *y)  
+        return -1;  
+  
+    return 0;  
+}  
+  
+int main()  
+{  
+    string str;  
+    int alphabet[26];  
+    for(int i=0; i<26; i++)  
+        alphabet[i]=0;  
+    getline(cin, str);  
+  
+    for(int i=0; i<str.length(); i++){  
+        if(isupper(str[i])) alphabet[str[i] - 'A']++;  
+        else if(islower(str[i])) alphabet[str[i] - 'a']++;  
+    }  
+    for(int i=0; i<26; i++){  
+        if(i<25)  
+            cout << alphabet[i] << " ";  
+        else cout << alphabet[i] << endl;  
+    }  
+    return 0;  
+}  
